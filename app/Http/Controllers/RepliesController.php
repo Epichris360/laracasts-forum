e5df46b95d0be4d9225da5f46d793bc032e73f3d
cristian <segoviacristian23@hotmail.com>
2020-10-03 14:17:00 -0400

    video-10

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -21,6 +21,7 @@ class RepliesController extends Controller
 
     public function store($channelId, Thread $thread) 
     {
+        $this->validate($request, [ 'body' => 'required' ]);
         $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
