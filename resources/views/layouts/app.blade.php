9befcb19fbb6eb6b76e311bc69c688fd83953cd4
cristian <segoviacristian23@hotmail.com>
2020-10-03 15:16:28 -0400

    video-12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/threads" >All Threads</a>
                         </li>
+                        <li class="nav-item">
+                            <a href="/threads/create" class="nav-link">New Thread</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Channels
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -8,18 +8,41 @@
                 <div class="card-header">Create a new thread</div>
 
                 <div class="card-body">
+                    @if(count($errors))
+                        <ul class="alert alert-danger">
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                     <form method="POST" action="/threads">
                         {{ csrf_field() }}
                         <div class="form-group">
+                            <label for="channel_id">Choose a channel</label>
+                            <select name="channel_id" id="channel_id" class="form-control" required >
+                                <option value="">Choose one...</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option 
+                                        value="{{ $channel->id }}"
+                                        {{ old('channel_id') == $channel->id ? 'selected' : '' }}
+                                    >
+                                        {{ $channel->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" name="title" id="title" class="form-control" >
+                            <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required >
                         </div>
                         <div class="form-group">
                             <label for="body">Body</label>
-                        <textarea name="body" id="body" rows="8" style="width: 100%;" class="form-control" ></textarea>    
+                        <textarea name="body" id="body" rows="8" style="width: 100%;" class="form-control" required >{{ old('body') }}</textarea>    
                         </div>
-
-                        <button type="submit" class="btn btn-primary" style="float: right;" >Publish</button>
+                        
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary" style="float: right;" >Publish</button>
+                        </div>            
                     </form>
                 </div>
             </div>
