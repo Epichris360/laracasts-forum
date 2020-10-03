4ef207558a6047df1f82fcbbd209726cbc637b3c
cristian <segoviacristian23@hotmail.com>
2020-10-02 21:14:42 -0400

    video-2-user-view-threads - on video 5 woops

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function path(){
+        return '/threads/' . $this->id;
+    }
+
+    public function replies() {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
